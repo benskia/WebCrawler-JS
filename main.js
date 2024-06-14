3f2a5bff0fb8a450641bc5078a1393c8508e0f07
benskia <benski.abraham@gmail.com>
2024-06-14 13:58:01 -0500

    update main to await crawlPage results

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require('./crawl.js')
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("Too few arguments: 'npm run BASE_URL'")
     process.exit()
@@ -11,7 +11,7 @@ function main() {
   }
   const baseURL = process.argv[2]
   console.log(`Starting crawl at ${baseURL}`)
-  const pages = crawlPage(baseURL)
+  const pages = await crawlPage(baseURL)
   for (const page of Object.entries(pages)) {
     console.log(page)
   }
