815485e68a7d32c39ea24a81a19c12e988318065
benskia <benski.abraham@gmail.com>
2024-06-13 19:05:33 -0500

    add argv control flow to main

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function main() {
-  pass
+  if (process.argv.length < 3) {
+    console.log("Too few arguments: 'npm run BASE_URL'")
+    process.exit()
+  }
+  if (process.argv.length > 3) {
+    console.log("Too many arguments: 'npm run BASE_URL'")
+    process.exit()
+  }
+  console.log(`Starting crawl at ${process.argv[2]}`)
 }
+
+main()
